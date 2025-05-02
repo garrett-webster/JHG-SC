da289a557a13730fdc52122e69402c13aeb0a953
Garrett Webster <garrettreeds@gmail.com>
2025-05-02 12:54:48 -0600

    Restore functionality that made the cause buttons stay blue when selected.

--- Client/combinedLayout/MainDocks.py
+++ Client/combinedLayout/MainDocks.py
@@ -208,7 +208,7 @@ class CornerContainer(QWidget):
                 background-color: #171717;
                 border: none;
             }
-            QScrollArea *[btnState="checked"] {
+            QScrollArea QPushButton[btnState="checked"] {
                 background-color: #2980b9;
                 color: #ecf0f1;
                 border: solid 2px #2980b9;
