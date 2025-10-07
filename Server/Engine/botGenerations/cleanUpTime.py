15f5288d1002ccfcea1b8c17f896c1888b48638c
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2025-10-07 13:07:40 -0600

    try those base boys tomorrow
    see if they work.

--- Server/Engine/botGenerations/cleanUpTime.py
+++ Server/Engine/botGenerations/cleanUpTime.py
@@ -13,23 +13,22 @@ generation_stats = {}
 if __name__ == "__main__":
     num_genes_new = 33
     num_genes_old = 40
+    num_pools = 3
     file_path = r"C:\Users\Sean Smith\Documents\GitHub\JHG-SC\Server\Engine\botGenerations\MelissasGenes.csv"
     all_new_genes = []
     with open(file_path, newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             words = row[0].split("_")
-            old_world = words.pop(0)
+            old_world = words.pop(0)  # assuming first element is a label
             new_gene = ""
-            for i in range(3): # num gene pools
-                for j in range(40): # going through all the old genes
-                    if j <= 33:
-                        index = j + (i*j)
-                        new_gene += "_" + str((words[index]))
-                    else:
-                        pass
-            new_string = "genes_" + new_gene
+            for i in range(num_pools):
+                for j in range(num_genes_new):  # only up to 33
+                    index = i * num_genes_old + j  # correct indexing across pools
+                    new_gene += "_" + str(words[index])
+            new_string = "genes" + new_gene
             all_new_genes.append(new_string)
+
     print("here are the new genes \n", all_new_genes)
 
 
